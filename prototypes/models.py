b1941e4d0d412f148d7f683199526ff0c51ea3ae
TaereenKim <kim.taereen@dnti.co.jp>
2026-06-08 18:13:47 +0900

    モデル

--- prototypes/models.py
+++ prototypes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Prototype(models.Model):
+  class Meta:
+    db_table = 'prototypes'
+
+  text = models.TextField(null=False, blank=False)
+  image = models.ImageField(upload_to='images/', blank=True, null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  
+
+
