0255b998276a878e67181bdfcb564b170dd58eac
TaereenKim <kim.taereen@dnti.co.jp>
2026-06-09 17:28:47 +0900

    6/9 17:28

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Comment
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404
+from prototypes.models import Prototype
 
-# Create your views here.
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    success_url = reverse_lazy('prototypes.detail')
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.user= self.request.user
+        comment.prototype = get_object_or_404(Prototype, pk=self.kwargs['pk'])
+        comment.save()
+        return super().form_valid(form)
