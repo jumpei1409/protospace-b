9c42cfe9c3428af5dbbceea4a203a52f23d80e15
jumpei1409 <miyata.junpei@dnti.co.jp>
2026-06-10 14:39:01 +0900

    detailのurl、view、modelの作成
    Comment.Forms.pyを作成する必要がある。

--- prototypes/urls.py
+++ prototypes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import IndexView
+from .views import IndexView,DetailView
 
 
 
 app_name = 'Prototypes'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
+    path('prototypes/<int:pk>/detail',DetailView.as_view(),name='detail'),
 ]
--- prototypes/views.py
+++ prototypes/views.py
@@ -1,7 +1,22 @@
 from .models import Prototype
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
+from Comment.forms import CommentForm
+from django.views.generic.edit import FormMixin
+from comments.models import Comment 
 
 class IndexView(ListView):
     model = Prototype
     template_name = 'prototypes/index.html'
     context_object_name = 'prototypes'
+
+class DetailView(FormMixin,DetailView):
+    model = Prototype
+    form_class = CommentForm
+    template_name = 'prototypes/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comments = Comment.objects.filter(prototype=self.object)
+        context['comments'] = comments
+        context['form'] = self.get_form()
+        return context
