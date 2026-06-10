c9fadf7b8df318733f8f3beca4438368872c6700
TaereenKim <kim.taereen@dnti.co.jp>
2026-06-10 10:48:02 +0900

    コメントhtml作成

--- comments/models.py
+++ comments/models.py
@@ -4,5 +4,5 @@ from django.conf import settings
 class Comment(models.Model):
     text = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    prototypes = models.ForeignKey('prototypes.Prototype', on_delete=models.CASCADE)
+    prototype = models.ForeignKey('prototypes.Prototype', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import Comment
 from .forms import CommentForm
 from django.shortcuts import get_object_or_404
@@ -9,11 +9,13 @@ from prototypes.models import Prototype
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentForm
-    success_url = reverse_lazy('prototypes.detail')
 
     def form_valid(self, form):
         comment = form.save(commit=False)
-        comment.user= self.request.user
+        comment.user = self.request.user
         comment.prototype = get_object_or_404(Prototype, pk=self.kwargs['pk'])
         comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('Prototypes:detail', kwargs={'pk': self.kwargs['pk']})
