32e83ac4e28cada924fd562a16d96c6795922fd4
TaereenKim <kim.taereen@dnti.co.jp>
2026-06-10 14:32:36 +0900

    ログインの入力欄の大きさ調整

--- prototypes/models.py
+++ prototypes/models.py
@@ -8,7 +8,7 @@ class Prototype(models.Model):
   title = models.CharField(max_length=100, null=False, blank=False)
   catchphrase = models.CharField(max_length=200, null=False, blank=False)
   concept = models.TextField(null=False, blank=False)
-  image = models.ImageField(upload_to='images/', blank=True, null=True)
+  image = models.ImageField(upload_to='images/', blank=False)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
   user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- prototypes/views.py
+++ prototypes/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
 from .models import Prototype
 from django.views.generic import ListView, CreateView
 from .forms import PrototypeForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(ListView):
     model = Prototype
     template_name = 'prototypes/index.html'
     context_object_name = 'prototypes'
 
-class CreateView(CreateView):
+class CreateView(LoginRequiredMixin, CreateView):
+    login_url = '/users/sign_in/'
     form_class = PrototypeForm
     template_name = 'prototypes/create.html'
     success_url = reverse_lazy('Prototypes:index')
@@ -19,4 +20,3 @@ class CreateView(CreateView):
         prototype.user = self.request.user
         prototype.save()
         return super().form_valid(form)
-    
