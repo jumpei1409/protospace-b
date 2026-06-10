fe27b7e84b4ecf1afe77ada27d6a413af5a302bf
TaereenKim <kim.taereen@dnti.co.jp>
2026-06-10 14:20:49 +0900

    実装条件による追加記述
    views、投稿するにはログイン必須
    models、画像必須

--- prototypes/views.py
+++ prototypes/views.py
@@ -2,13 +2,15 @@ from .models import Prototype
 from django.views.generic import ListView, CreateView
 from .forms import PrototypeForm
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(ListView):
     model = Prototype
     template_name = 'prototypes/index.html'
     context_object_name = 'prototypes'
 
-class CreateView(CreateView):
+class CreateView(LoginRequiredMixin, CreateView):
+    login_url = '/users/sign_in/'
     form_class = PrototypeForm
     template_name = 'prototypes/create.html'
     success_url = reverse_lazy('Prototypes:index')
