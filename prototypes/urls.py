627663b57f2b2a82cc78ba85d0b9df76377a5ef5
limeimei-hub <li.kenmei@dnti.co.jp>
2026-06-10 15:20:41 +0900

    編集画面までのルーティングの設定

--- prototypes/urls.py
+++ prototypes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
-from .views import IndexView
+from .views import IndexView, PrototypeUpdateView
 
 
 
 app_name = 'Prototypes'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
+    path('prototypes/<int:pk>/update', PrototypeUpdateView.as_view(), name='update'),
 ]
