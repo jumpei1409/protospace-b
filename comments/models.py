44aeb0aa09188bd0f38e5dd69a3471b20b5992d3
jumpei1409 <miyata.junpei@dnti.co.jp>
2026-06-11 13:36:33 +0900

    update models.py
    textフィールドにバリデーションを追加

--- comments/models.py
+++ comments/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 class Comment(models.Model):
-    text = models.TextField()
+    text = models.TextField(null = False, blank=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     prototype = models.ForeignKey('prototypes.Prototype', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
