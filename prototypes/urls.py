ca2472cdf75e2c6271dcda0425c3eeb35979b735
limeimei-hub <li.kenmei@dnti.co.jp>
2026-06-11 11:06:26 +0900

    投稿までのルーティングを修正

--- prototypes/urls.py
+++ prototypes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import IndexView, CreateView
+from .views import IndexView, PrototypeCreateView
 
 app_name = 'Prototypes'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('create/', CreateView.as_view(), name='create'),
+    path('create/', PrototypeCreateView.as_view(), name='create'),
 ]
