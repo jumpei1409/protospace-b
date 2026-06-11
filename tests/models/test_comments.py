acfb9f3bc8d5629dc1d6b2462d63182a70ea854e
jumpei1409 <miyata.junpei@dnti.co.jp>
2026-06-11 14:46:15 +0900

    コメントファイル修正
    テスト修正

--- tests/models/test_comments.py
+++ tests/models/test_comments.py
@@ -2,21 +2,22 @@ from django.test import TestCase
 from django.urls import reverse
 from prototypes.models import Prototype
 from users.models import CustomUser
+from comments.models import Comment
 
 class CommentCreateViewTest(TestCase): 
-    def Setup(self):
-         self.user = CustomUser.objects.create.user(
+    def setUp(self):
+         self.user = CustomUser.objects.create_user(
             email='test@test.com',
             nickname='テストユーザー',
             password='testpass123',
     )
-        self.prototype = Prototype.objects.create(
+         self.prototype = Prototype.objects.create(
             title='テストタイトル',
             catchphrase='テストテキスト',
-            concept='テストコンセプト'
+            concept='テストコンセプト',
             user=self.user,
     )
-        self.url = reverse( Comment:create, kwargs={'pk': self.prototype.pk})
+         self.url = reverse('Comment:create', kwargs={'pk': self.prototype.pk})
 
 # コメントがDBに保存されるか
     def test_comment_saved_to_db(self):
@@ -33,9 +34,10 @@ class CommentCreateViewTest(TestCase):
 # ログイン時だけフォームが表示されるか
     def test_form_shown_when_logged_in(self):
         self.client.force_login(self.user)
-        response = self.client.post('Prototypes:detail')
-        self.assertContains(response,)
+        response = self.client.get(reverse('Prototypes:detail',kwargs={'pk': self.prototype.pk}))
+        self.assertContains(response,'送信する')
 
 # 未ログインはフォームが表示されないか
     def test_form_hidden_when_logged_out(self):
-        response = self.client.get()
+        response = self.client.get(reverse('Prototypes:detail', kwargs={'pk': self.prototype.pk}))
+        self.assertNotContains(response, '送信する')
